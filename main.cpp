b68a5477ebcd073feb0fc594698e66ba48cf3ca0
necrelox <necrelox@proton.me>
2023-09-27 10:25:56 +0200

    ✨ Add try catch in main

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@
 int main(int ac, char **av) {
     if (ac != 2)
         return -1;
-    std::string path = av[1];
-    WhiteSpaceRemover remover(path, true, std::regex(".*"));
-    std::cout << "Total spaces removed : " << remover.run() << std::endl;
+    try {
+        std::string path = av[1];
+        WhiteSpaceRemover remover(path, true, std::regex(".*"));
+        std::cout << "Total spaces removed : " << remover.run() << std::endl;
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
     return 0;
 }
